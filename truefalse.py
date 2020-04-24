cc9342983d0ecfdd34c5b8614cda5ab03a94e23b
ErikDanisovszky <danisovszkyerik7@gmail.com>
2020-04-24 12:26:57 +0200

    truthvaluse, and if challenge

---
+++ truefalse.py
@@ -0,0 +1,17 @@
+day = "Friday"
+temperature = 30
+raining = False
+
+if (day == "Saturday" and temperature > 27) or not raining:
+    print("Go swimming")
+else:
+    print("Learn Python")
+
+cipomarka = "adiddas"
+meret = 46
+ar = 15000
+
+if cipomarka == "adiddas" and meret == 46 and ar < 20000:
+    print("Ez a cipo jó rád és meg is veheted")
+else:
+    print("Nincs rád való cipellő")
---
+++ truthvalues.py
@@ -0,0 +1,12 @@
+if 0:
+    print("True")
+else:
+    print("False")
+
+
+name = input("Please enter your name: ")
+# if name: equals if name != "":
+if name != "":
+    print("Hello, {}".format(name))
+else:
+    print("Are you the man witn no name?")
